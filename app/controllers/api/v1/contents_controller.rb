1b5fd8437e9374c5beb73bee12e9f1bf977b724c
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-02-16 09:19:29 +0900

    [add]create/deleteアクション作成vue側から投稿と削除が可能

--- app/controllers/api/v1/contents_controller.rb
+++ app/controllers/api/v1/contents_controller.rb
@@ -4,11 +4,29 @@ class Api::V1::ContentsController < ApplicationController
   end
   
   def create
+    content = Content.new(content_params)
+    if content.save
+      render json: { status: 200, content: content, message: 'success' }
+    else
+      render json: { status: 403, content: nil, message: content.errors }
+    end
   end
 
   def destroy
+    content = Content.find(params[:id])
+    if content.destroy
+      render json: { status: 200, message: 'success' }
+    else
+      render json: { status: 403, message: content.errors }
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def content_params
+    params.permit(:comment)
   end
 end
