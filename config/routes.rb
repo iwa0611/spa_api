ac7305347f686a8a52f2c57361e3db7cb80eeb23
ryo_iwamoto <daimoto7211@yahoo.co.jp>
2022-03-02 09:46:17 +0900

    [add]devise関係ファイル追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/search', to: 'contents#search', defaults: { format: :json }
       resources :contents, only: [:index, :create, :destroy, :edit]
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 end
