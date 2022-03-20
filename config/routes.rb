c8ecc526db918484d0c75c23d5474967f90754a9
いわもとりょう <iwamotoryou@iwamotoryounoMacBook-Air.local>
2022-03-20 09:49:29 +0900

    [update]search_indexアクションとルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/search', to: 'contents#search', defaults: { format: :json }
+      post '/search_index', to: 'contents#search_index', defaults: { format: :json }
       resources :contents, only: [:index, :create, :destroy, :update, :show]
       mount_devise_token_auth_for 'User', at: 'auth'
     end
